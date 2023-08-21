3e2e5ff861d38979f3ddd4f34570b36685ad4ed2
Akash S <akashajay510@gmail.com>
2023-08-21 16:22:25 +0530

    day4

---
+++ src/main/java/com/example/demo/controller/ApiController.java
@@ -0,0 +1,28 @@
+package com.example.demo.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.model.Employee;
+import com.example.demo.service.ApiService;
+
+@RestController
+public class ApiController {
+	@Autowired
+	ApiService apser;
+	@PostMapping("post")
+	public Employee adding(@RequestBody Employee ep)
+	{
+		return apser.saveinfo(ep);
+	}
+	@GetMapping("get")
+	public List<Employee> showing()
+	{
+		return apser.showinfo();
+	}
+}
---
+++ src/main/java/com/example/demo/service/ApiService.java
@@ -0,0 +1,24 @@
+package com.example.demo.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.model.Employee;
+import com.example.demo.repository.EmployeeRepo;
+
+@Service
+public class ApiService {
+	@Autowired
+	EmployeeRepo em;
+	public Employee saveinfo(Employee e)
+	{
+		return em.save(e);
+	}
+	public List<Employee> showinfo()
+	{
+		return em.findAll();
+	}
+
+}
